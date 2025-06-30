27625c231e108556aebd2d606d4dce04312ab9f5
Julian Mateu <julianmateu@monzo.com>
2025-06-30 23:44:35 +0100

    Fix nvm

--- install.sh
+++ install.sh
@@ -459,8 +459,8 @@ install_nvm() {
     fi
 
     export NVM_DIR="${HOME}/.nvm"
-    [ -s "${NVM_DIR}/nvm.sh" ] && \. "${NVM_DIR}/nvm.sh"  # This loads nvm
-    [ -s "${NVM_DIR}/bash_completion" ] && \. "${NVM_DIR}/bash_completion"  # This loads nvm bash_completion
+    [ -s "${NVM_DIR}/nvm.sh" ] && source "${NVM_DIR}/nvm.sh"  # This loads nvm
+    [ -s "${NVM_DIR}/bash_completion" ] && source "${NVM_DIR}/bash_completion"  # This loads nvm bash_completion
 }
 
 # setup_node - Install Node.js LTS version
